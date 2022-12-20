dce43fbac05fbe1486f9e1e5eaae1abfa70eed2f
joetsai <yltsai0609@gmail.com>
2022-12-20 17:47:07 +0800

    Adds test to make sure 5 + 5 is equal to 10

--- main_test.py
+++ main_test.py
@@ -1,8 +1,11 @@
-from main import Add  
-  
-def TestAdd():  
-        assert Add(2,3) == 5  
-        print("Add Function works correctly")  
-  
-if __name__ == '__main__':  
-        TestAdd()  
+from main import Add
+
+
+def TestAdd():
+    assert Add(2, 3) == 5
+    assert Add(5, 5) == 10
+    print("Add Function works correctly")
+
+
+if __name__ == "__main__":
+    TestAdd()
