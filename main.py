3580d32d3561d9d4eaf2c5441062fa6b3b6997d2
yltsai0609 <yltsai0609@gmail.com>
2023-08-27 09:29:28 +0800

    new commit reids

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b  
           
 def SayHello():  
-        print("sup world from Rithik")  
+        print("sup world from abc")  
   
 if __name__ == '__main__':  
         SayHello()  
