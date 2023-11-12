77c0f860c686555cf25b4c1cff34089081ebb226
yltsai0609 <yltsai0609@gmail.com>
2023-11-12 09:55:43 +0800

    fix/fix ci and add bq, redis

--- ml_projects/house_price_prediction/transform_c.py
+++ ml_projects/house_price_prediction/transform_c.py
@@ -1,2 +1,21 @@
+from cicd.bq import BQ
+import redis
+
+bq = BQ()
+
+def test_bq(bq):
+    sql = 'SELECT * FROM `bigquery-public-data.covid19_aha.hospital_beds` LIMIT 1'
+    rows = bq.query(sql)
+    assert len(rows) >= 1
+    print('we can R Bigquery!')
+
+def test_redis():
+    redis.Redis().flushall()
+    k, v = 'k', 'v'
+    r = redis.Redis(host='localhost',port=6379,db=0)
+    r.set(k,v)
+    assert r.get(k) == b'v'
+    print('we can R/W Redis!')
+
 print('some-change')
 print('transform_c starts')
